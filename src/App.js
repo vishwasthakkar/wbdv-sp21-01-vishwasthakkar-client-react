e5f829dd62c2f1f4d60aede6d884fffec2598834
vishwasthakkar <thakkar.vi@husky.neu.edu>
2021-02-21 17:49:42 -0500

    Added Home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./components/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <div className="container-fluid">
+          <Route path="/" exact={true}  component={Home}/>
+          {/*<Route path="/courses" component={CourseManager}/>*/}
+          {/*<Route path="/editor" component={CourseEditor}/>*/}
+          {/*<Route path="/editor" exact={true} render={(props) => <CourseEditor {...props}/>}/>*/}
+          {/*<div className="container-fluid">*/}
+          {/*  <CourseManager/>*/}
+          {/*  <CourseEditor/>*/}
+          {/*</div>*/}
+        </div>
+      </BrowserRouter>
   );
 }
 
