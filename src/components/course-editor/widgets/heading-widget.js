a67dc605e61b128604b9929a3f82530f82a1eb3a
vishwasthakkar <thakkar.vi@husky.neu.edu>
2021-03-21 16:36:22 -0400

    Basic Assignment implemented!

--- src/components/course-editor/widgets/heading-widget.js
+++ src/components/course-editor/widgets/heading-widget.js
@@ -12,17 +12,17 @@ const HeadingWidget = ({
     return (
         <div>
             { !editing &&
-                <span>
+                <>
                     {widget.size === 1 && <h1>{widget.text}</h1>}
                     {widget.size === 2 && <h2>{widget.text}</h2>}
                     {widget.size === 3 && <h3>{widget.text}</h3>}
                     {widget.size === 4 && <h4>{widget.text}</h4>}
                     {widget.size === 5 && <h5>{widget.text}</h5>}
                     {widget.size === 6 && <h6>{widget.text}</h6>}
-                    <i onClick={() => setEditing(true)} className="fas fa-cog float-right">
+                    <span><i onClick={() => setEditing(true)} className="fas fa-cog float-right">
                         &nbsp;
-                    </i>
-                </span>
+                    </i></span>
+                </>
             }
 
             {
@@ -30,7 +30,7 @@ const HeadingWidget = ({
                 <div>
                     <select
                         onChange={
-                            (e) => setCache(widget => ({...cache, type: e.target.value}))
+                            (e) => setCache({...cache, type: e.target.value})
                         }
                         value={cache.type}
                         className="form-control"
---
+++ src/components/course-editor/widgets/paragraph-widget.js
@@ -0,0 +1,65 @@
+import React, {useState, useEffect} from 'react';
+
+const ParagraphWidget = ({
+     widget,
+     updateWidget,
+     deleteWidget
+}) => {
+    const [editing, setEditing] = useState(false);
+    const [cache, setCache] = useState(widget);
+    return (
+        <div>
+            {
+                editing &&
+                <>
+                    <select
+                        onChange={
+                            (e) => setCache({...cache, type: e.target.value})
+                        }
+                        value={cache.type}
+                        className="form-control"
+                    >
+                        <option value={"HEADING"}>Heading</option>
+                        <option value={"PARAGRAPH"}>Paragraph</option>
+                    </select>
+
+                    <textarea
+                        onChange={(e) => setCache({...cache, text: e.target.value})}
+                        value={cache.text}
+                        className="form-control">
+                    </textarea>
+                    <i onClick={() => {
+                        setEditing(false);
+                        updateWidget(cache)}
+                    }
+                       className="fas fa-check float-right"
+                    >
+                        &nbsp;
+                    </i>
+                    <i onClick={() => {
+                        setEditing(false);
+                        deleteWidget(widget)}
+                    }
+                       className="fas fa-times float-right">
+                        &nbsp;
+                    </i>
+                </>
+            }
+
+            {
+                !editing &&
+                <>
+                    <p>
+                        {cache.text}
+                    </p>
+                    <i onClick={() => setEditing(true)}
+                       className="fas fa-cog float-right">
+                        &nbsp;
+                    </i>
+                </>
+            }
+        </div>
+    )
+}
+
+export default ParagraphWidget
